1509514b7bbbc35e3c25b68492f72797123f0534
sp <solene.poydenot@gmail.com>
2024-10-17 18:56:48 +0200

    Changes to be committed: modified:   powershell/blanchissement.ps1 modified:   powershell/liste-reseau/regex-ps.json

--- powershell/blanchissement.ps1
+++ powershell/blanchissement.ps1
@@ -13,54 +13,79 @@ $liste_reseau = "sfr","redhat"
 function get_reseau {
     return Read-Host -Prompt "Entrez le nom du réseau concerné parmi $liste_reseau"
 }
+function get_path {
+    return Read-Host -Prompt "Entrez le chemin vers le dossier/fichier à blanchir"
+}
+function test_path($path) {
+    if (-Not (Test-Path $path)) {
+        return $false
+    }
+}
+
+# Définition du fichier output
+$date = (Get-Date -format "yyyyMMdd")
+$output_path = ([string](Get-Location)+"\sortie_blanchissement\"+$date)
+if (test_path($output_path) == $false) {
+    mkdir $output_path
+}
+
 $reseau = get_reseau
 if (($liste_reseau -contains $reseau) -eq $false) {
     Write-Error -Message "Le reseau n'est pas dans la liste des réseaux disponibles ($liste_reseau)"
     $reseau = get_reseau
 }
 
-# Récupération de la liste des serveurs propre au réseau choisi
-$server_list_path = (Get-ChildItem -Path (Get-Location) -File -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -match "$reseau" })
-$server_list = (Get-Content $server_list_path)
 
 # Récupération de la liste des regex indépendemment du réseau
 $regex = (Get-Content (Get-ChildItem -Path (Get-Location) -File -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -match "regex-ps" })) | Out-String | ConvertFrom-Json
 $liste_regex = ((Get-Content (Get-ChildItem -Path (Get-Location) -File -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -match "regex-ps" })) | Out-String | ConvertFrom-Json | Get-Member | Where-Object { $_.MemberType -match "NoteProperty"}).Name
 
+# Récupération de la liste des serveurs propre au réseau choisi et création d'une regex particulière
+$server_regex = "("+[string]::Join(")|(",((Get-ChildItem -Path (Get-Location) -File -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Name -match "$reseau" }) | Get-Content))+")"
+# Ajout dans la liste des regex
+$regex | Add-Member -Name 'user_servers' -Type NoteProperty -Value $server_regex
+$liste_regex += "user_servers"
+
 # Récupération du chemin vers le dossier à blanchir
-function get_path {
-    return Read-Host -Prompt "Entrez le chemin vers le dossier/fichier à blanchir"
-}
 $path = get_path
-if (-Not (Test-Path $path)) {
+if (test_path($path) == $false) {
     Write-Error -Message "Le chemin $path n'existe pas, veuillez rentrer un chemin existant"
     $path = get_path
 }
 
+# Copie des fichiers à blanchir
+Write-Output "Copie des fichiers à blanchir vers $output_path"
+Try {
+    Copy-Item -Recurse $path -Destination $output_path -errorAction stop
+    Write-Host "Fichiers copiés."
+} Catch {
+    Write-Error "Il y a déjà ces dossiers dans $output_path"
+    $choice = Read-Host -Prompt "Voulez-vous les écraser ? (o/N)"
+    if ( $choice -eq "o") {
+        Copy-Item -Recurse $path -Destination $output_path -Force
+        Write-Host "Fichiers écrasés et copiés."
+    } else {
+        Write-Host "Sortie du programme de blanchissement."
+        Break
+    }
+}
+
+$output_path += "\"+$path.Split("\")[-1]
 # Blanchissement des fichiers
-Write-Output "Fichiers blanchis dans $path :"
+Write-Output "Blanchissement des fichiers de $path dans $output_path :"
 # Lister les fichiers et dossiers de manière récursive
-Get-ChildItem -r  $path | ForEach-Object { 
+Get-ChildItem -r  $output_path | ForEach-Object { 
     # Modifier uniquement les fichiers et pas les dossiers
     if ($_.PsIsContainer -eq $false) { 
-        Write-Output "Working on : "(Get-Item $_.FullName | Resolve-Path -Relative)
+        Write-Output "Working on : " ([string](Get-Item $_.FullName | Resolve-Path -Relative))
+
         # Pour chaque entrée de la liste des regex génériques
-        for ($i = 0; $i -lt $liste_regex.Length; $i+=1) {
+        for ($i = 0; $i -lt $liste_regex.Length; $i+=1) 
+        {
             $key = $liste_regex[$i]
             $occurances = (((Get-Content $_.FullName) -match $regex.($key)) | Measure-Object -Line).Lines
 
             (Get-Content $_.FullName) -replace $regex.($key), ($key) | Set-Content $_.FullName
-
-            Write-Output " => Replaced $occurances occurances of $key"
-        }
-
-        # Pour chaque entrée dans la liste des serveurs propre au réseau
-        for ($i = 0; $i -lt $server_list.Length; $i+=1) {
-            # Modifier les noms de serveur dans la liste
-            $key = $server_list[$i]
-            $occurances = (((Get-Content $_.FullName) -match $key) | Measure-Object -Line).Lines
-
-            (Get-Content $_.FullName) -replace $key, [string]"SERVER_$i" | Set-Content $_.FullName
 
             Write-Output " => Replaced $occurances occurances of $key"
         }
